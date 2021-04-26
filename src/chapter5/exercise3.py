7adb17a368356d202133c2a8344268e8b77bc8fe
groupx1bse2 <franciswetaka@yahoo.com>
2021-04-26 08:03:01 +0300

    Assigment:chapter 5 exercise

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -1,33 +1,49 @@
 import sys
-#vending machine algorithm
+# vending machine algorithm
 def centds(a):
-    #function for breaking the price into dollars and cents
+    # function for breaking the price into dollars and cents
     x = float(a)//1
     y = (float(a) % 1)*100
     print('Amount due is: ', int(x),'dollars and ',int(y) ,'cents')
+
+def coins(a):
+    # function for allocation of coin/bills to be entered by user
+    if a == 'q' or a == 'Q':
+        x = 0.25
+    elif a == 'd' or a == 'D':
+        x = 0.1
+    elif a == 'n' or a == 'N':
+        x = 0.05
+    elif a == 'o' or a == 'O':
+        x = 1
+    elif a == 'f' or a == 'F':
+        x = 5
+    else:
+        pass
+    return x
 
 nickels = 25
 dimes = 25
 quarters = 25
 ones = 0
 fives = 0
-
+total = 0
 print('                  Welcome to vending machine change maker                 ')
 print('--------------------------------------------------------------------------')
 print('****Stock contains****')
-#content of stock present
+# content of stock present
 print(nickels, 'Nickels')
 print(dimes, 'Dimes')
 print(quarters, 'Quarters')
 print(ones, 'Ones')
 print(fives, 'Fives')
 
-#Area for checking validity of price input
-#Request for purchase price from the user.
+# Area for checking validity of price input
+# Request for purchase price from the user.
 
 while True:
         price1 = input('Enter purchase price or "q" to quit: ')
-        #try:
+
         if str(price1) == 'q' or str(price1) == 'Q':
                 sys.exit()
         try:
@@ -39,7 +55,7 @@ while True:
         except:
             print('Invalid input, try again!!')
 
-#Section for deposits made.
+# Section for deposits made.
 
 print('----------------------------------------------------------------------')
 print('Menu of deposits:')
@@ -53,47 +69,31 @@ print("'c' - cancel deposit")
 centds(price1)
 
 menu_list = ['q','d','n','o','f','c']
-#verifing if input is correct
+# verifying if input is correct
 while True:
     deposit = input('Indicate your deposit: ')
-    if str(deposit) in menu_list:
+    if deposit in menu_list:
+        if deposit == menu_list[5]:
+            total = (total + coins(deposit))//0.25
+            print(total, 'Quarter')
+        if coins(deposit) < float(price1):
+            total = total + coins(deposit)
+            if total > float(price1):
+                total = total - float(price1)
+                print(int(total//0.25), 'Quarters')
+            elif total == float(price1):
+                print('No change')
+            continue
+        if coins(deposit) > float(price1):
+            total = total + coins(deposit)
+            total = total - float(price1)
+            print(': ', int(total//0.25), ' : Quarters')
+        if coins(deposit) == float(price1):
+            total = total + coins(deposit)
+            print('No change')
+
         break
     else:
         print('illegal entry, try again!!')
-q = quarters
-d = dimes
-n = nickels
-o = ones
-f = fives
-c = 'c'
 
-if deposit == 'q':
-    deposit = q
-elif deposit == 'd':
-    deposit =d
-elif deposit == 'n':
-    deposit = n
-elif deposit == 'o':
-    deposit = o
-elif deposit == 'f':
-    deposit = f
-#elif deposit == 'c':
-#    deposit = c
 
-while True:
-    if deposit == float(price1):
-        print('The is same as:', deposit)
-    elif deposit == menu_list[5]:
-        print('coucil')
-        #call the change dispenser function: to be continued
-    elif deposit > float(price1):
-        print("Deposit is greater", deposit)
-        #call the change dispenser function: to be continued
-    elif deposit < float(price1):
-        print('deposit is lesser', deposit)
-        #call change dispenser function: to be continued
-
-        #call change dispenser function: to be continued
-    break
-
-print('Program to be continues....')
